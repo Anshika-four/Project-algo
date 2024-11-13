959760ea6e3cfcec5fc67d78fa5c1bd6732c240a
Anshika Sharma <anshikasharma0282@gmail.com>
2024-11-13 12:25:51 +0530

    deleted

--- src/Tools/InformationOfAlgorithms&Presentation/InformationMain.js
+++
@@ -1,5 +0,0 @@
-let information={
-    name: "Anshika Sharma",
-    information:"This Algorithm Visualizer is an interactive tool designed to help users understand the inner workings of complex algorithms by visualizing their step-by-step processes. It provides a hands-on way to explore and learn about several types of algorithms, including sorting algorithms like Bubble Sort, Quick Sort, and Merge Sort, as well as essential graph algorithms like Dijkstra’s shortest path, Depth-First Search (DFS), and Breadth-First Search (BFS). \n I Hope you find this helpful in understanding the working of several algorithms ",
-};
-export default information;
